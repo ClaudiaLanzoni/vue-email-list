a53d31d279f0fcc9b35a4c84b44d8fc67ea186b2
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-28 18:39:31 +0200

    finalized exercise

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,117 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div v-if="indirizzo1 != '' && indirizzo2 != '' && indirizzo3 != '' && indirizzo4 != '' 
+    && indirizzo5 != '' && indirizzo6 != '' && indirizzo7 != '' && indirizzo8 != '' 
+    && indirizzo9 != '' && indirizzo10 != ''">
+      <ul>
+        <li>{{indirizzo1.response}}</li>
+        <li>{{indirizzo2.response}}</li>
+        <li>{{indirizzo3.response}}</li>
+        <li>{{indirizzo4.response}}</li>
+        <li>{{indirizzo5.response}}</li>
+        <li>{{indirizzo6.response}}</li>
+        <li>{{indirizzo7.response}}</li>
+        <li>{{indirizzo8.response}}</li>
+        <li>{{indirizzo9.response}}</li>
+        <li>{{indirizzo10.response}}</li>
+      </ul>
+    </div>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
+  },
+
+  data : function () { //REGOLA COMPONENT
+    return {
+      indirizzo1 : "",
+      indirizzo2 : "",
+      indirizzo3 : "",
+      indirizzo4 : "",
+      indirizzo5 : "",
+      indirizzo6 : "",
+      indirizzo7 : "",
+      indirizzo8 : "",
+      indirizzo9 : "",
+      indirizzo10 : "",
+    }
+  },
+
+  mounted() { //This hook is not called during server-side rendering.
+
+    console.log(axios);
+    
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo1 = res.data;
+      //console.log(res.data);
+    });
+
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo2 = res.data;
+      //console.log(res.data);
+    });
+    
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo3 = res.data;
+      //console.log(res.data);
+    });
+
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo4 = res.data;
+      //console.log(res.data);
+    });
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo5 = res.data;
+      //console.log(res.data);
+    });
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo6 = res.data;
+      //console.log(res.data);
+    });
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo7 = res.data;
+      //console.log(res.data);
+    });
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo8 = res.data;
+      //console.log(res.data);
+    });
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo9 = res.data;
+      //console.log(res.data);
+    });
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => {
+      
+      this.indirizzo10 = res.data;
+      //console.log(res.data);
+    });
   }
 }
 </script>
@@ -24,5 +124,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  ul {
+    list-style-type: none;
+  }
 }
 </style>
