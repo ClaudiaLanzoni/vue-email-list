e77ce2105e7e9c16a415a368c7050daf29bb3d64
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-30 00:00:35 +0200

    cycle for, data indirizzi

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
 
-    <div v-if="indirizzo1 != '' && indirizzo2 != '' && indirizzo3 != '' && indirizzo4 != '' 
-    && indirizzo5 != '' && indirizzo6 != '' && indirizzo7 != '' && indirizzo8 != '' 
-    && indirizzo9 != '' && indirizzo10 != ''">
+    <div>
       <ul>
-        <li>{{indirizzo1.response}}</li>
-        <li>{{indirizzo2.response}}</li>
-        <li>{{indirizzo3.response}}</li>
-        <li>{{indirizzo4.response}}</li>
-        <li>{{indirizzo5.response}}</li>
-        <li>{{indirizzo6.response}}</li>
-        <li>{{indirizzo7.response}}</li>
-        <li>{{indirizzo8.response}}</li>
-        <li>{{indirizzo9.response}}</li>
-        <li>{{indirizzo10.response}}</li>
+        <li v-for="(element, index) in indirizzi" :key="index">
+          {{element.response}}
+        </li>
       </ul>
     </div>
   
@@ -32,86 +23,21 @@ export default {
 
   data : function () { //REGOLA COMPONENT
     return {
-      indirizzo1 : "",
-      indirizzo2 : "",
-      indirizzo3 : "",
-      indirizzo4 : "",
-      indirizzo5 : "",
-      indirizzo6 : "",
-      indirizzo7 : "",
-      indirizzo8 : "",
-      indirizzo9 : "",
-      indirizzo10 : "",
+      indirizzi : []
     }
   },
 
   mounted() { //This hook is not called during server-side rendering.
 
-    console.log(axios);
-    
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(res => {
       
-      this.indirizzo1 = res.data;
-      //console.log(res.data);
-    });
+      this.indirizzi.push(res.data);
+      //console.log(indirizzo);
 
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo2 = res.data;
-      //console.log(res.data);
     });
-    
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo3 = res.data;
-      //console.log(res.data);
-    });
-
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo4 = res.data;
-      //console.log(res.data);
-    });
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo5 = res.data;
-      //console.log(res.data);
-    });
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo6 = res.data;
-      //console.log(res.data);
-    });
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo7 = res.data;
-      //console.log(res.data);
-    });
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo8 = res.data;
-      //console.log(res.data);
-    });
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo9 = res.data;
-      //console.log(res.data);
-    });
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
-      
-      this.indirizzo10 = res.data;
-      //console.log(res.data);
-    });
+    }
   }
 }
 </script>
